fbbb763b54aabe6837821ba8981db2c7abbda23d
Dev Patel <109800062+learndev0509@users.noreply.github.com>
2022-09-20 15:31:05 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Extra places for collectstatic to find static files.
