603090bc27b3a20aebe6fb1a72a26e75fc076c06
Dev Patel <109800062+learndev0509@users.noreply.github.com>
2022-09-20 15:37:42 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,10 +120,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
